b2d0d38f9e906f3d5b796064ad0a8528b459c72b
Thoai Adam Nguyen <nguyenphithoaihung@gmail.com>
2023-09-20 22:14:53 -0500

    updated wrangle

--- AdamWine/wrangle.py
+++ AdamWine/wrangle.py
@@ -1,23 +1,26 @@
 import os
+
 import pandas as pd
-import numpy as np
-import seaborn as sns
 
-
-def read_wine_data():
+def read_and_combine_wine_data():
     """
-    Read the red and white wine DataFrames from the provided URLs.
+    Read the red and white wine DataFrames from the provided URLs, add a 'wine_type' column to each,
+    and combine them into one DataFrame.
 
     Returns:
-    - red_df: DataFrame containing red wine data.
-    - white_df: DataFrame containing white wine data.
+    - df: Combined DataFrame containing both red and white wine data.
     """
+    # Read red and white wine DataFrames
     red_url = 'https://query.data.world/s/km3v7y3hbhnq6q3qxmgunsty22chem?dws=00000'
     white_url = 'https://query.data.world/s/tzchvcxc66f2wiye4k3x3agz2vthyu?dws=00000'
-    
     red_df = pd.read_csv(red_url)
     white_df = pd.read_csv(white_url)
-    
-    return red_df, white_df
 
-red_wine_data, white_wine_data = df()
+    # Add a 'wine_type' column to each DataFrame
+    red_df['wine_type'] = 'red'
+    white_df['wine_type'] = 'white'
+
+    # Combine the DataFrames into one
+    df = pd.concat([red_df, white_df], ignore_index=True)
+
+    return df
